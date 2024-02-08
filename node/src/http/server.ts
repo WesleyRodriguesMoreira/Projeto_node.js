7accb6103b22037e4cd8aeec706311f89a472585
Wesley Rodrigues <wesleyrodriguesmoreira02@gmail.com>
2024-02-07 23:42:54 -0300

    6° commit
    Criação da página de votos

--- node/src/http/server.ts
+++ node/src/http/server.ts
@@ -1,10 +1,19 @@
 import fastify from 'fastify';
+import cookie from '@fastify/cookie';
 import { createPoll } from '../http/routes/create-poll';
+import { getPoll } from '../http/routes/get-poll';
+import { votOnPoll } from '../http/routes/vot-on-polls';  
 
 const app = fastify();
 
-app.register(createPoll);
+app.register(cookie, {
+    secret: "polls-app-nlw", // === Signature/security key ===
+    hook: 'onRequest',
+});
 
+app.register(createPoll);
+app.register(getPoll);
+app.register(votOnPoll);
 
 
 app.listen({port: 3333}).then(() => {
